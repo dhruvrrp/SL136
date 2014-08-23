9d16a86a0d916827f0f723b00a824a2ed6b2f148
dhruvrrp <dkaushal@ucsd.edu>
2014-08-22 17:32:17 -0700

    Created POCO objects

---
+++ POCO/Instructor.cs
@@ -0,0 +1,23 @@
+﻿namespace POCO
+{
+    using System.Collections.Generic;
+
+    public class Instructor
+    {
+        public string InstructorId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Title { get; set; }
+
+        public override string ToString()
+        {
+            return this.InstructorId + "-"
+                + this.FirstName + "-"
+                + this.LastName + "-"
+                + this.Title;
+        }
+    }
+}
---
+++ POCO/TA.cs
@@ -0,0 +1,20 @@
+﻿namespace POCO
+{
+    using System.Collections.Generic;
+
+    public class TA
+    {
+        public int TAId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return this.TAId + "-"
+                + this.FirstName + "-"
+                + this.LastName;
+        }
+    }
+}
