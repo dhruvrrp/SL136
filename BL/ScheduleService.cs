5c9497b902dffcb732ad6e6b385eeb90f2bb7cbd
Dustin Hu <shamHu@gmail.com>
2014-08-22 18:46:16 -0700

    added functions to ICourseRepo and IScheduleRepo, added methods (Insert, Update, Delete) to CourseService.cs

--- BL/ScheduleService.cs
+++ BL/ScheduleService.cs
@@ -1,9 +1,8 @@
 ﻿namespace Service
 {
+    using System;
     using System.Collections.Generic;
-
     using IRepository;
-
     using POCO;
 
     public class ScheduleService
@@ -13,6 +12,45 @@
         public ScheduleService(IScheduleRepository repository)
         {
             this.repository = repository;
+        }
+
+        public void InsertSchedule(Schedule schedule, ref List<string> errors)
+        {
+            if (schedule == null)
+            {
+                errors.Add("Schedule cannot be null");
+                throw new ArgumentException();
+            }
+
+            this.repository.InsertCourseSchedule(schedule, ref errors);
+        }
+
+        public void UpdateSchedule(Schedule schedule, ref List<string> errors)
+        {
+            if (schedule == null)
+            {
+                errors.Add("Schedule cannot be null");
+                throw new ArgumentException();
+            }
+
+            if (schedule.Course == null)
+            {
+                errors.Add("Invalid course id");
+                throw new ArgumentException();
+            }
+
+            this.repository.UpdateCourseSchedule(schedule, ref errors);
+        }
+
+        public void DeleteSchedule(string id, ref List<string> errors)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                errors.Add("Invalid course id");
+                throw new ArgumentException();
+            }
+
+            this.repository.DeleteCourseSchedule(id, ref errors);
         }
 
         public List<Schedule> GetScheduleList(string year, string quarter, ref List<string> errors)
