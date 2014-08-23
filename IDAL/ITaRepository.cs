4aa05fceba7c122222a9509e23b836d6af697a80
dhruvrrp <dkaushal@ucsd.edu>
2014-08-22 18:02:09 -0700

    Changed the TA repo interface to match new definitions

--- IDAL/ITaRepository.cs
+++ IDAL/ITaRepository.cs
@@ -6,18 +6,18 @@
 
     public interface ITARepository
     {
-        void InsertTA(int idOfTA, string firstName, string lastName, ref List<string> errors);
+        void InsertTA(TA ta, ref List<string> errors);
 
-        void UpdateTaInfomation(int idOfTA, string firstName, string lastName, ref List<string> errors);
+        void UpdateTA(TA ta, ref List<string> errors);
 
-        void DeleteTa(int id, ref List<string> errors);
+        void DeleteTA(int id, ref List<string> errors);
 
-        System.Tuple<string, string> GetTaInfo(int id, ref List<string> errors);
+        TA GetTaInfo(int id, ref List<string> errors);
 
         void AssignTaToClass(int idOfTA, int scheduleID, ref List<string> errors);
 
         void RemoveTaFromClass(int idOfTA, int scheduleID, ref List<string> errors);
 
-        List<System.Tuple<string, string>> ViewTasForClass(int scheduleId, ref List<string> errors);
+        List<TA> ViewTasForClass(int scheduleId, ref List<string> errors);
     }
 }
